8f3242efa5291e69dd25372b84ea650a457121d2
swarup <126848179+Swa-DevOps@users.noreply.github.com>
2024-02-19 14:40:09 +0530

    Update sonar-scan.sh

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -2,4 +2,4 @@
 export PATH=/opt/apache-maven-3.9.6/bin:$PATH
 export JAVA_HOME=/opt/jdk-17.0.2
 mvn clean install
-mvn clean package sonar:sonar -Dsonar.host.url="http://13.201.33.163:9000" -Dsonar.projectKey=AEM-WEEKEND-7 -Dsonar.projectName='AEM-WEEKEND-7' -Dsonar.token=sqa_a51390374c149203754221835b8232248ed0d2cc
+mvn clean package sonar:sonar -Dsonar.host.url="http://13.232.104.36:9000" -Dsonar.projectKey=AEM-WEEKEND-8 -Dsonar.projectName='AEM-WEEKEND-8' -Dsonar.token=sqa_a51390374c149203754221835b8232248ed0d2cc
